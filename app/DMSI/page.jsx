a8a953f13cca282103d2491a9c14b7a5a3ab6cb3
BORJA MEDINA RAMOS <72421634+ShayBRJ@users.noreply.github.com>
2023-12-17 11:08:09 +0000

    Leccion 5 realizada

---
+++ app/DMSI/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function DMSI() {
+  return (
+    <main>
+      <h2>DMSI</h2>
+      <div className="card">
+        <h3>¿Qué es DMSI?</h3>
+        <p>Es el conjunto de prácticas y disciplinas que involucran la creación, evolución y gestión de sistemas de información.</p>
+      </div>
+    </main>
+    
+  )
+}
